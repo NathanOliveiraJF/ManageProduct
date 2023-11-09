e102b59fa1b1c06dfaf9e25c209c73f17fc25aa5
Nathan Oliveira <nathanoliveira003jf@gmail.com>
2023-11-09 14:02:32 -0300

    wip

--- tests/Requests/Products/CreateProductRequestTest.php
+++ tests/Requests/Products/CreateProductRequestTest.php
@@ -3,108 +3,48 @@
 
 namespace Tests\Requests\Products;
 
-use App\Http\Requests\Products\CreateProductRequest;
+use Config\DI\Builder;
 use PHPUnit\Framework\TestCase;
 
 class CreateProductRequestTest extends TestCase {
-  protected $formRequest;
+  protected $createProductRequest;
 
   protected function setUp(): void
   {
     parent::setUp();
-    $this->formRequest = new CreateProductRequest();
+    $this->createProductRequest = Builder::getContainer("CreateProductRequest");
   }
 
   /** @test */
-  public function testShouldCreateAProductSuccessfully(): void
+  public function testValidProduct(): void
   {
     $payload = [
-      "sku" => "12312312",
-      "name" => "productTest",
+      "sku" => "12279567",
+      "name" => "Bebida de Arroz",
       "price" => 100.0,
-      "description" => "descriptionTest",
-      "quantity" => 100
+      "description" => "bebida de arroz",
+      "quantity" => 10000 
     ];
 
-    $this->formRequest->validated($payload);
-    $isvalid = $this->formRequest->isValid();
-
+    $this->createProductRequest->validated($payload);
+    $isvalid = $this->createProductRequest->isValid();
     $this->assertTrue($isvalid);
   }
 
   /** @test */
-  public function testShouldFailValidationWhenCreatingProductWithoutSku(): void
+  public function testInvalidProduct(): void
   {
     $payload = [
       "sku" => "",
-      "name" => "productTest",
-      "price" => 100.0,
-      "description" => "descriptionTest",
-      "quantity" => 100
-    ];
-    $this->formRequest->validated($payload);
-    $isNotvalid =$this->formRequest->isValid();
-    $this->assertFalse($isNotvalid);
-  }
-
-  /** @test */
-  public function testShouldFailValidationWhenCreatingProductWithoutName(): void
-  {
-    $payload = [
-      "sku" => "231231212",
       "name" => "",
-      "price" => 100.0,
-      "description" => "descriptionTest",
-      "quantity" => 100
-    ];
-    $this->formRequest->validated($payload);
-    $isNotvalid =$this->formRequest->isValid();
-    $this->assertFalse($isNotvalid);
-  }
-
-  /** @test */
-  public function testShouldFailValidationWhenCreatingProductWithoutPrice(): void
-  {
-    $payload = [
-      "sku" => "231231212",
-      "name" => "productTest",
-      "price" => 0,
-      "description" => "descriptionTest",
-      "quantity" => 100
-    ];
-    $this->formRequest->validated($payload);
-    $isNotvalid =$this->formRequest->isValid();
-    $this->assertFalse($isNotvalid);
-  }
-
-  /** @test */
-  public function testShouldFailValidationWhenCreatingProductWithoutDescription(): void
-  {
-    $payload = [
-      "sku" => "231231212",
-      "name" => "productTest",
-      "price" => 100,
+      "price" => "",
       "description" => "",
-      "quantity" => 100
+      "quantity" => ""
     ];
-    $this->formRequest->validated($payload);
-    $isNotvalid =$this->formRequest->isValid();
-    $this->assertFalse($isNotvalid);
-  }
 
-  /** @test */
-  public function testShouldFailValidationWhenCreatingProductWithoutQuantity(): void
-  {
-    $payload = [
-      "sku" => "231231212",
-      "name" => "productTest",
-      "price" => 100,
-      "description" => "descriptionTest",
-      "quantity" => 0  
-    ];
-    $this->formRequest->validated($payload);
-    $isNotvalid =$this->formRequest->isValid();
-    $this->assertFalse($isNotvalid);
+    $this->createProductRequest->validated($payload);
+    $isNotValid = $this->createProductRequest->isValid();
+    $this->assertTrue($isNotValid);
   }
 }
 
